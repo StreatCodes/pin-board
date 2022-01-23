c5b1a0cb310f9f7d57385ba0295d677631563ce5
Matthew Streatfield <matt.r.streatfield@gmail.com>
2022-01-24 00:00:24 +1100

    clean up styling, pass menuItem text as prop

--- src/fileExplorer.tsx
+++ src/fileExplorer.tsx
@@ -1,5 +1,5 @@
 import { h, Component, render, FunctionalComponent } from "preact";
-import { useRef, useState } from "preact/hooks"
+import { useEffect, useRef, useState } from "preact/hooks"
 
 import { FolderIcon, NewFileIcon, PlusIcon } from "./icons"
 import { MenuItem } from "./menuItem";
@@ -9,6 +9,17 @@ import "./fileExplorer.css";
 export const FileExplorer: FunctionalComponent = () => {
     const [expanded, setExpanded] = useState(false);
     const contentRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            const ele = e.target as Element;
+            if (!contentRef.current.contains(ele)) {
+                setExpanded(false);
+            }
+        }
+        window.addEventListener('click', handler);
+        return () => window.removeEventListener('click', handler)
+    }, [expanded])
 
     let expandedStyle = '';
     if (contentRef.current && expanded) {
@@ -23,15 +34,15 @@ export const FileExplorer: FunctionalComponent = () => {
                 <PlusIcon />
             </div>
 
-            <MenuItem>
-                <FolderIcon />
-                <div>New Folder</div>
+            <MenuItem text="New Folder">
+                <FolderIcon width={20} height={20} />
             </MenuItem>
 
-            <MenuItem>
-                <NewFileIcon />
-                <div>Create New Board</div>
+            <MenuItem text="Create New Board">
+                <NewFileIcon width={20} height={20} />
             </MenuItem>
+
+            <hr />
 
             <section>
                 <div style="width: 24px; height: 24px; border-radius: 50%; background-color: gray"></div>
--- src/menuItem.tsx
+++ src/menuItem.tsx
@@ -2,12 +2,14 @@ import { h, Component, render, FunctionalComponent, ComponentChildren } from "pr
 import "./menuItem.css"
 
 interface Props {
+    text: string;
     children?: ComponentChildren;
     onClick?: () => void;
 }
 
-export const MenuItem: FunctionalComponent<Props> = ({ children, onClick }: Props) => {
+export const MenuItem: FunctionalComponent<Props> = ({ text, children, onClick }: Props) => {
     return <div class="menu-item" onClick={onClick}>
         {children}
+        <div class="text">{text}</div>
     </div>
 }
